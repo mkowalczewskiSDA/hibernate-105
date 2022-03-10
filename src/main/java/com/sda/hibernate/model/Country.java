20eb59a5710c6511d793be27401c88f853a89955
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2022-03-10 21:05:30 +0100

    first drafts

--- src/main/java/com/sda/hibernate/model/Country.java
+++ src/main/java/com/sda/hibernate/model/Country.java
@@ -1,4 +1,20 @@
 package com.sda.hibernate.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
 public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CO_ID")
+    private int id;
+    @Column(name = "CO_NAME")
+    private String name;
+    @Column(name = "CO_ALIAS")
+    private String alias;
+
 }
--- src/main/java/com/sda/hibernate/przyklad1/Main.java
+++ src/main/java/com/sda/hibernate/przyklad1/Main.java
@@ -1,4 +1,16 @@
 package com.sda.hibernate.przyklad1;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
+
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.close();
+        sessionFactory.close();
+
+    }
 }
