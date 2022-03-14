58a4e49631e7ddb10c225c6e9c684b21c6c236ff
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2022-03-14 18:28:33 +0100

    Hibernate Util

--- src/main/java/com/sda/hibernate/model/Country.java
+++ src/main/java/com/sda/hibernate/model/Country.java
@@ -12,12 +12,12 @@ public class Country {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "CO_ID")
-    private int id;
+    private Integer id;
     @Column(name = "CO_NAME")
     private String name;
     @Column(name = "CO_ALIAS")
     private String alias;
 
-    
+
 
 }
--- src/main/java/com/sda/hibernate/przyklad2/Main.java
+++ src/main/java/com/sda/hibernate/przyklad2/Main.java
@@ -1,12 +1,25 @@
 package com.sda.hibernate.przyklad2;
 
+import com.sda.hibernate.model.Country;
+import com.sda.hibernate.util.HibernateUtil;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        usePersist();
     }
 
     public static void usePersist() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Country country = new Country();
+        country.setAlias("IN");
+        session.beginTransaction();
+        session.persist(country);
+        country.setName("India");
+        session.getTransaction().commit();
+        session.close();
 
     }
 
