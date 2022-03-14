60a2aa3d811fe72a7e7715c0b2d109ac32311037
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2022-03-14 18:52:05 +0100

    persist

--- src/main/java/com/sda/hibernate/przyklad2/Main.java
+++ src/main/java/com/sda/hibernate/przyklad2/Main.java
@@ -2,7 +2,6 @@ package com.sda.hibernate.przyklad2;
 
 import com.sda.hibernate.model.Country;
 import com.sda.hibernate.util.HibernateUtil;
-import org.hibernate.Hibernate;
 import org.hibernate.Session;
 
 public class Main {
@@ -16,8 +15,15 @@ public class Main {
         Country country = new Country();
         country.setAlias("IN");
         session.beginTransaction();
+        //persist dodaje nam obiekt do persistance contextu - pod spodem dokonuje się "insert"
+        //nasz obiekt country dostaje swój numer id
         session.persist(country);
+        //jeżeli obiekt jest już spersistowany, a my go zmieniamy, to podczas wykonywania
+        //commita dokona się Update
         country.setName("India");
+        //jeżeli mamy bardzo dużo wierszy do dodania, i chcemy część tej operacji już wykonać
+        //(żeby np zwolnić trochę pamięci, to możemy wykonać flush)
+        //zapisuje nam transakcję
         session.getTransaction().commit();
         session.close();
 
