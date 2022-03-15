71f6bf951f3cf17132550a133b3610ffe7455be8
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2022-03-15 20:58:28 +0100

    OrderDaoImpl example + how to utilize interface in tests

--- src/main/java/com/sda/hibernate/przyklad5/Main.java
+++ src/main/java/com/sda/hibernate/przyklad5/Main.java
@@ -1,7 +1,11 @@
 package com.sda.hibernate.przyklad5;
 
+import com.sda.hibernate.dao.GenericDao;
+import com.sda.hibernate.dao.OrderDao;
+import com.sda.hibernate.dao.OrderDaoImpl;
 import com.sda.hibernate.model.Address;
 import com.sda.hibernate.model.Country;
+import com.sda.hibernate.model.Order;
 import com.sda.hibernate.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
@@ -20,6 +24,24 @@ public class Main {
         addressSet.forEach(System.out::println);
         session.close();
         //System.out.println(addressSet);
+        OrderDao orderDao = new OrderDaoImpl();
+        /* przykład po
+        OrderDao orderDaoTest = new OrderDao() {
+            @Override
+            public List<Order> findByDate() {
+                return List.of(new Order());
+            }
+
+            @Override
+            public Order findById(int id) {
+                return null;
+            }
+
+            @Override
+            public void insertObject(Order order) {
+
+            }
+        };*/
     }
 
 }
