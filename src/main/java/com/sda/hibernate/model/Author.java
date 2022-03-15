40b3f3ab9218e1158813ef4797af68463c65c400
Michal Kowalczewski <m.kowalczewski2@gmail.com>
2022-03-15 17:30:55 +0100

    many to many

--- src/main/java/com/sda/hibernate/model/Author.java
+++ src/main/java/com/sda/hibernate/model/Author.java
@@ -1,9 +1,24 @@
 package com.sda.hibernate.model;
 
-import javax.persistence.Id;
+import lombok.Data;
 
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
 public class Author {
 
     @Id
     private Integer id;
+    private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "books_authors",
+            joinColumns = {@JoinColumn(name = "BO_ID")},
+            inverseJoinColumns = {@JoinColumn(name = "AU_ID")}
+    )
+    private Set<Book> books;
+
+
 }
--- src/main/java/com/sda/hibernate/model/Book.java
+++ src/main/java/com/sda/hibernate/model/Book.java
@@ -1,4 +1,20 @@
 package com.sda.hibernate.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+@Entity
+@Data
 public class Book {
+
+    @Id
+    private Integer id;
+    private String name;
+    @ManyToMany(fetch = FetchType.LAZY, mappedBy = "books")
+    private Set<Author> authors;
 }
